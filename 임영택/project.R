3cc96055e7fdd91d17568a6ee27aba00d7947600
kso8868 <58205165+kso8868@users.noreply.github.com>
2019-12-12 20:08:26 +0900

    1212

--- 임영택/project.R
+++ 임영택/project.R
@@ -32,24 +32,31 @@ year <- 2016:2020
 par(mfrow = c(2,2))
 
 # 1년 예산
-ggplot(yesan, aes(x = year , y = yesan$총예산_10만)) +
+ggplot(yesan, aes(x = year , y = yesan$총예산_억)) +
   geom_line(color = 'black') +
-  ggtitle("제주도 1년 예산 (단위 :십만)")+
+  ggtitle("제주도 1년 예산 (단위 : 억)")+
   theme(plot.title = element_text(size = 25, #글자사이즈
                                   face = "bold", # 글자 폰트
                                   colour = "steelblue")) + # 글자 색상
   labs( x = "년도" , y = "제주예산") +
-  geom_text(aes(label = yesan$총예산_10만,
+  geom_text(aes(label = yesan$총예산_억,
                 vjust = -1.1,
                 hjust = 0)) +
-  annotate(geom='text', x=2018, y=4500, size=13,
-           label='17년 이랑 비교시 18년 예산이 13억이 오름')
+  annotate(geom='text', x=2018, y=45000, size=13,
+           label='17년 이랑 비교시 18년 예산이 13조원 증가') +
+  annotate("rect", # 사각형 표시
+           xmin = 2016.9,
+           xmax = 2018.15,
+           ymin = 36000,
+           ymax = 52000,
+           alpha = 0.3, # 투명도
+           fill = "skyblue")
 
 # 문화예산
 
 ggplot(yesan, aes(x = year , y = yesan$문화및관광)) +
   geom_line(color = 'blue') +
-  ggtitle("제주도 1년 문화예산 (단위 :십만)")+
+  ggtitle("제주도 1년 문화예산 (단위 :억)")+
   theme(plot.title = element_text(size = 25, #글자사이즈
                                   face = "bold", # 글자 폰트
                                   colour = "steelblue")) + # 글자 색상
@@ -57,8 +64,8 @@ ggplot(yesan, aes(x = year , y = yesan$문화및관광)) +
   geom_text(aes(label = yesan$문화및관광,
                 vjust = -1.1,
                 hjust = 0.5)) +
-  annotate(geom='text', x=2018, y=260, size=13,
-           label='17년 이랑 비교시 18년 문화예산이 3천7백만원 오름')
+  annotate(geom='text', x=2018, y=2600, size=13,
+           label='17년 이랑 비교시 18년 문화예산이 3백 70억원 증가')
 
 # 증감률
 
