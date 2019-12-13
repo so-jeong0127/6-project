9b50e791e9a9d0e5a49099cd39ddbe1ea62ae027
kso8868 <58205165+kso8868@users.noreply.github.com>
2019-12-13 13:08:23 +0900

    1213

--- 최종자료정리/김대현/gni-방문객비교.R
+++ 최종자료정리/김대현/gni-방문객비교.R
@@ -1,8 +1,12 @@
+library(ggplot2)
 gni <- read.csv("gniaa.csv")
+gni1 <- read.csv("gniaazzzz.csv")
 peo <- read.csv("제주1.csv")
 
 p <- c(peo)
 g <- c(gni)
+
+
 
 visitor<-as.numeric(p)
 
@@ -16,3 +20,29 @@ plot(GNI~visitor, data = pg, main = "방문자수와 GNI 상관관계")
 res <- lm(GNI~visitor, data = pg)
 res
 abline(res, col = 'red')
+
+
+
+
+
+yearz <- c('16년1분기','16년2분기','16년3분기','16년4분기',
+           '17년1분기','17년2분기','17년3분기','17년4분기',
+           '18년1분기','18년2분기','18년3분기','18년4분기')
+
+zzz <- data.frame(yearz)
+gni12 <- as.vector(gni1)
+zd <- as.character(yearz)
+zzz1 <- data.frame(zd,gni1)
+str(zd)
+str(zzz1)
+
+ggplot(zzz1, aes(x = zzz1$zd, y = zzz1$GNI)) +
+  geom_line()
+
+
+
+
+
+
+
+
